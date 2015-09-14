78da80fbf6a18d7eee4285ee2700b80c608cdc1e
Safirn <Safir.najafi@gmail.com>
2015-09-14 16:04:12 +0200

    first commit

---
+++ src/lab1/concordence/Main.java
@@ -0,0 +1,15 @@
+package lab1.concordence;
+
+import java.io.IOException;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		ConcordenceSetup conc = new ConcordenceSetup();
+		conc.setupConcordence();
+	}
+}
---
+++ src/lab1/concordence/Searcher.java
@@ -0,0 +1,11 @@
+package lab1.concordence;
+
+public class Searcher {
+	public Searcher() {
+		
+	}
+	
+	public void searchWord(String word) {
+		
+	}
+}
